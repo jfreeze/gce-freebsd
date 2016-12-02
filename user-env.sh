0546f5e4c02f42737fee0bc6e4452fc1a345be76
User Jimfreeze <jimfreeze@elixirconf-webserver.c.elixirconf-project.internal>
2016-12-02 22:24:06 +0000

    added updates for .shrc and .profile

--- user-env.sh
+++ user-env.sh
@@ -7,4 +7,19 @@ sed -E -i '' 's/^# umask	022/umask 022/' .shrc
 sed -E -i '' "s/# alias cp='cp -ip'/alias cp='cp -ip'/" .shrc
 sed -E -i '' "s/# alias mv='mv -i'/alias  mv='mv -i'/" .shrc
 sed -E -i '' "s/# alias rm='rm -i'/alias  rm='rm -i'/" .shrc
+sed '/alias g/ a \
+alias d=\"ls -alFG\"
+' .shrc > /tmp/shrc
+mv /tmp/shrc .shrc
 
+
+cat << EOF >> .shrc
+# set prompt: ``username@hostname$ ''
+PS1="\`whoami\`@\`hostname | sed 's/\..*//'\`"
+case \`id -u\` in
+ 	0) PS1="\${PS1}# ";;
+ 	*) PS1="\${PS1}$ ";;
+esac
+EOF
+
+cp /usr/share/skel/dot.profile .profile

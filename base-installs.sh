6923cbf83ee0afe37f6ebf6c4aa7c7660407d9d4
User Jimfreeze <jimfreeze@elixirconf-webserver.c.elixirconf-project.internal>
2016-12-02 22:25:23 +0000

    added base installs

---
+++ base-installs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo pkg install -y wget
+sudo pkg install -y vim-lite
+
+# For build and deploy machines, install Elixir
+sudo pkg install -y elixir
+
+sudo pkg install -y nginx
+sudo pkg install -y git  
+
+

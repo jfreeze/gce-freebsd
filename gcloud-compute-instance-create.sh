bdf8a2637b883a87c59af59007a8728dda1c578b
Jim Freeze <jimfreeze@jimsdevmbp.attlocal.net>
2016-12-12 20:47:30 -0600

    updates

--- gcloud-compute-instance-create.sh
+++ gcloud-compute-instance-create.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
 
-CMD=$(cat <<EOF
-gcloud compute instances create ${2} \
-  --project ${1} \
-  --zone "us-central1-a" \
-  --machine-type "f1-micro" \
-  --network "${1}-net" \
-  --tags "pingable,sshable,http-server" \
-  --image freebsd-11-0-release-amd64 \
-  --image-project=freebsd-org-cloud-dev \
-  --boot-disk-device-name "${2}-bootdisk" \
-  --no-address \
-  --boot-disk-size "40" 
+usage ()
+{
+  echo "Usage : $(basename "$0") project-name instance-name network-name"
+  exit
+}
+
+if [ "$#" -ne 3 ]
+then
+  usage
+fi
+
+run () {
+  CMD=$(cat <<EOF
+  gcloud compute instances create ${2} \
+    --project "${1}" \
+    --zone "us-central1-a" \
+    --machine-type "f1-micro" \
+    --tags "pingable,sshable,http-server,https-server" \
+    --subnet "${3}" \
+    --image freebsd-11-0-release-amd64 \
+    --image-project=freebsd-org-cloud-dev \
+    --boot-disk-device-name "${2}-bootdisk" \
+    --no-address \
+    --boot-disk-size "40" 
 EOF
 )
 
-#  --subnet "${1}-net" \
-echo "${CMD}"
-echo "${CMD}" | /bin/sh
+  echo ${CMD}
+  #echo "${CMD}" | /bin/sh
+}
+
+run $1 $2 $3

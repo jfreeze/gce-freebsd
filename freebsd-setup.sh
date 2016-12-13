7f9389b0b7441932d2de626d010983a8139c7f5d
User Jimfreeze <jimfreeze@elixirconf-webserver.c.elixirconf-project.internal>
2016-12-13 16:11:33 +0000

    updates

--- freebsd-setup.sh
+++ freebsd-setup.sh
@@ -16,9 +16,16 @@ echo "Installing web applications..."
 curl https://raw.githubusercontent.com/jfreeze/gce-freebsd/master/web-installs.sh > /tmp/web-installs.sh
 /bin/sh /tmp/web-installs.sh
 
+echo "Copying extra config files..."
+curl https://raw.githubusercontent.com/jfreeze/gce-freebsd/master/chg-shell-to-bash.sh > /tmp/chg-shell-to-bash.sh
+
 echo "Configuring nginx..."
 curl https://raw.githubusercontent.com/jfreeze/gce-freebsd/master/nginx-setup.sh > /tmp/nginx-setup.sh
 chmod 755 /tmp/nginx-setup.sh
+
+echo
+echo "Run /tmp/chg-shell-to-bash.sh for compatability with edeliver"
+echo 
 echo "Nginx needs DOMAIN manually setup."
 echo "Configure /tmp/nginx-setup.sh and run /tmp/nginx-setup.sh to complete"
 
